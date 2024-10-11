60975c912f95240118b5d3e3da976899c822bd7a
Jason Roberts <jason@vastpuddle.com.au>
2024-10-11 15:33:23 +1100

    carrier service step 1; chore: Add Shippy Wippy Delivery checkout UI extension

---
+++ app/extensions/checkout-ui/src/index.js
@@ -0,0 +1,43 @@
+import {
+    extend,
+    BlockStack,
+    Button,
+    Heading,
+    Text,
+    useApi,
+    useTranslate,
+} from '@shopify/checkout-ui-extensions-react';
+
+extend('Checkout::DeliveryAddress::RenderAfter', (root) => {
+    const translate = useTranslate();
+    const { delivery, shop } = useApi();
+
+    root.appendChild(
+        root.createComponent(
+            BlockStack,
+            {},
+            [
+                root.createComponent(Heading, {}, "Shippy Wippy Delivery"),
+                root.createComponent(
+                    Button,
+                    {
+                        onPress: async () => {
+                            const deliveryGroups = await delivery.getDeliveryGroups();
+                            const products = deliveryGroups.flatMap(group => group.deliveryOptions)
+                                .flatMap(option => option.merchandise);
+
+                            // Here you would implement the logic to calculate shipping based on product dimensions
+                            // For now, we'll just log the products
+                            console.log('Products:', products);
+
+                            // You would then use the delivery.updateDeliveryOption() method to update the shipping option
+                        },
+                    },
+                    "Calculate Shipping"
+                ),
+            ]
+        )
+    );
+
+    return root;
+});
